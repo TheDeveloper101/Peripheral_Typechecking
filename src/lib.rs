35daab68beee873a427b2eb48d65d0dfaef8b544
Sai Divvela <saidivvela@sais-air.umd.edu>
2024-09-22 13:33:11 -0400

    got code to compile and currently working on refactoring to be more principled

--- src/lib.rs
+++ src/lib.rs
@@ -1,55 +1,52 @@
-use std::any::Any;
-use std::collections::{hash_map, HashMap};
-use std::iter::Map;
+use std::collections::HashMap;
 
 use proc_macro::TokenStream;
-use proc_macro2::{Span, TokenStream};
 use quote::{quote, ToTokens};
 use syn::spanned::Spanned;
 use syn::visit_mut::{visit_expr_method_call_mut, VisitMut};
-use syn::{parse_macro_input, Error, ExprMethodCall, Ident};
+use syn::{parse_macro_input, ExprMethodCall, Ident};
 
+#[derive(PartialOrd, Ord, PartialEq, Eq, Hash)]
 enum USBType {
-    Uninit,
-    Idle,
-    Ready,
-    Run
+    Uninit(EndpointType),
+    Idle(EndpointType),
+    Ready(EndpointType),
+    Run(EndpointType),
+    Unused(EndpointType)
 }
-struct Gamma {
-    context: HashMap<syn::Ident, (USBType, USBType)>,
-    state: USBType,
-    transitions: HashMap<USBType, USBType>,
-    error: Some(String)
+
+#[derive(PartialOrd, Ord, PartialEq, Eq, Hash)]
+enum EndpointType {
+    Blah
+}
+
+#[derive(PartialOrd, Ord, PartialEq, Eq, Hash)]
+enum Entry {
+    Variable(syn::Ident),
+    Function(syn::Ident)
+}
+struct Context {
+    gamma: HashMap<Entry, (USBType, USBType)>,
+    error: Option<String>
 }
 
 #[proc_macro_attribute]
-fn typecheck(args: TokenStream, input: TokenStream) -> TokenStream {
+pub fn typecheck(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let g: Gamma = Gamma {context: HashMap::new(),
-                    state: USBType::Uninit,
-                    transitions: HashMap::from([
-                        (USBType::Uninit, USBType::Idle),
-                        (USBType::Idle, USBType::Ready),
-                        (USBType::Ready, USBType::Run),
-                        (USBType::Run, USBType::Idle),
-                        (USBType::Run, USBType::Uninit),
-                        (USBType::Idle, USBType::Idle),
-                        (USBType::Ready, USBType::Ready),
-                        (USBType::Run, USBType::Run)
-                        ]),
+    let mut c: Context = Context {
+                    gamma: HashMap::new(),
                     error: None
                 };
     let mut ast = parse_macro_input!(input as syn::Expr);
     let mut ret: proc_macro2::TokenStream = quote! {#ast};
-    g.visit_expr_mut(ast);
-    match g.error {
+    c.visit_expr_mut(&mut ast);
+    match c.error {
         None => {}
         Some(message) => {
             ret.extend(
                 syn::Error::new(
                     ast.span(), 
-                    format!("Improper API usage!", 
-                    )
+                    format!("{message}")
                 )
                 .to_compile_error()
             )
@@ -58,24 +55,34 @@ fn typecheck(args: TokenStream, input: TokenStream) -> TokenStream {
     ret.into()
 }
 
-impl VisitMut for Gamma {
+impl VisitMut for Context {
     fn visit_expr_method_call_mut(&mut self, i: &mut ExprMethodCall) {
-        let method_type = *self.context.get(&i.method).unwrap();
-        if(method_type.0 == state) {
-            let end = self.transitions.get(&i.method);
-            match end {
-                Some(t) => {
-                    self.state = method_type.1;
-                    visit_expr_method_call_mut(self, i)
-                }
-                None => {
-                    self.error = Some("Improper API Usage!".to_string());
-                    return;
+        let method_type = self.gamma.get(&Entry::Function(i.method.clone())).unwrap();
+        if let syn::Expr::Path(ref path) = *i.receiver {
+            if let Some(ident) = path.path.get_ident() {
+                if method_type.0 == self.gamma.get(&Entry::Variable(ident.clone())).unwrap().0 {
+                    let end = self.gamma.get(&Entry::Function(i.method.clone()));
+                    match end {
+                        Some(t) => {
+                            visit_expr_method_call_mut(self, i)
+                        }
+                        None => {
+                            self.error = Some("Improper API Usage!".to_string());
+                            return;
+                        }
+                    }
                 }
             }
+        }
+    }
+}
+
+impl USBType {
+    fn join(t1: USBType, t2: USBType) -> USBType {
+        if t1 < t2 {
+            t2
         } else {
-            self.error = Some("Improper API Usage!".to_string());
-            return;
+            t1
         }
     }
 }
